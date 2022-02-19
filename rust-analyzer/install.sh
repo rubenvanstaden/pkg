dd046f81e7f3205a5caea357a5902ea89940c247
ruben <rubenvanstaden@gmail.com>
2022-02-18 21:11:54 -0500

    feat: add rust-analyzer:

---
+++ rust-analyzer/install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -ex
+
+PKG_NAME="nvim"
+PKG_VERSION="2022-02-14"
+SRC_PATH="$HOME/.local/src"
+BIN_PATH="$HOME/.local/bin"
+
+cd /tmp
+wget "https://github.com/rust-analyzer/rust-analyzer/archive/refs/tags/$PKG_VERSION.tar.gz"
+tar -zxvf "$PKG_VERSION.tar.gz"
+
+mv "rust-analyzer-$PKG_VERSION" "$SRC_PATH/$PKG_NAME-$PKG_VERSION"
+cd "$SRC_PATH/$PKG_NAME-$PKG_VERSION"
+cargo xtask install --server
